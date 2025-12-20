112119831c952b7d96780d8dba36c92573ca5ff4
kaviyaranjitham <kit28.24bcb026@gmail.com>
2025-12-20 10:50:56 +0530

    Create a new project-Name: CKART

---
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.repository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ecommerce.entity.User;
+public class UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByEmail(String email);
+    public Boolean existsByUsername(String username);
+    public Boolean existsByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.username = ?1 OR u.email = ?2")
+    public Optional<User> findByUsernameOrEmail(String username, String email);
+
+}
